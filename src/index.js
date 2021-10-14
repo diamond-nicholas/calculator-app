474499cd5ba232888d2c1a216680f67c90401cee
Diamond Nicholas <diamondnicholas154@gmail.com>
2021-10-14 05:06:03 +0100

    fix all eslint bug

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import PropTypes from 'prop-types';
 import Big from 'big.js';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
