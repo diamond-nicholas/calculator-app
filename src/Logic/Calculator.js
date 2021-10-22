48ec5c0f52ad22f4200656b964c96aacee6d850f
Diamond Nicholas <diamondnicholas154@gmail.com>
2021-10-22 03:57:25 +0100

    implement the operate logic

--- src/Logic/Calculator.js
+++ src/Logic/Calculator.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Big from 'big.js';
+import Operate from './Operate';
 
-const Calculator = () => (
-  <div>
-    <p>hello world</p>
-  </div>
-);
+const Calculator = (calculatorData, buttonName) => {
+  const data = calculatorData;
+
+  if (data.Operate === '=') {
+    data.total = null;
+    data.next = null;
+    data.Operation = null;
+  }
+};
 
 export default Calculator;
--- src/Logic/Operate.js
+++ src/Logic/Operate.js
@@ -1,9 +1,25 @@
-import React from 'react';
+/* eslint-disable default-case */
+import Big from 'big.js';
 
-const Operate = () => (
-  <div>
-    <h2>hello world</h2>
-  </div>
-);
+const Operate = (numberOne, numberTwo, operation) => {
+  const a = Big(numberOne);
+  const b = Big(numberTwo);
+  switch (operation) {
+    case '+':
+      return a.plus(b).toString;
+    case '-':
+      return a.minus(b).toString;
+    case 'x':
+      return a.times(b).toString;
+    case '%':
+      return a.times(b.div(100)).toString;
+    case '÷':
+      if (b.eq('0')) {
+        return 'Error';
+      }
+      return a.div(b).toString;
+  }
+  return {};
+};
 
 export default Operate;
