3ec933ef26b875911026ba9728355e96a4283831
Diamond Nicholas <diamondnicholas154@gmail.com>
2021-10-22 04:14:47 +0100

    implement the case +/- logic in calc js

--- src/Logic/Calculator.js
+++ src/Logic/Calculator.js
@@ -1,3 +1,5 @@
+/* eslint-disable default-case */
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import Big from 'big.js';
 import Operate from './Operate';
@@ -8,7 +10,24 @@ const Calculator = (calculatorData, buttonName) => {
   if (data.Operate === '=') {
     data.total = null;
     data.next = null;
-    data.Operation = null;
+    data.operation = null;
+  }
+  let result;
+  switch (buttonName) {
+    case 'AC':
+      data.total = null;
+      data.next = null;
+      data.operaion = null;
+      return data;
+    case '+/-':
+      if (data.operation && data.next) {
+        result = Big(data.next).times(-1).toString();
+        data.next = result;
+        return data;
+      }
+      result = Big(data.total).times(-1).toString();
+      data.total = result;
+      return data;
   }
 };
 
