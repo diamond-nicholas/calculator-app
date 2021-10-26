1772c5826a5a17a1fc0492c592bff3c3811abb21
Diamond Nicholas <diamondnicholas154@gmail.com>
2021-10-26 05:59:49 +0100

    implement button.js component, add the handleClick function

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import propTypes from 'prop-types';
 
 const Button = (props) => {
-  const { buttonName } = props;
+  const { buttonName, clickHandler } = props;
+  const handleClick = () => (clickHandler(buttonName));
   return (
     <div>
       {buttonName}
