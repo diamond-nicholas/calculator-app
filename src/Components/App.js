49211d5194f5264df7a13b394463253e2c0ce572
Diamond Nicholas <diamondnicholas154@gmail.com>
2021-10-26 05:25:00 +0100

    changed app component to class

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,16 +1,29 @@
+/* eslint-disable react/prefer-stateless-function */
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { Component } from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 import Calculator from '../Logic/Calculator';
 
-function App() {
-  return (
-    <>
-      <Display />
-      <ButtonPanel />
-    </>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <Display />
+        <ButtonPanel />
+      </>
+    );
+  }
 }
 
 export default App;
